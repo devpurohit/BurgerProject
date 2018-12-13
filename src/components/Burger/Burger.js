4ffce32840bd1cda4e091abbbb54c0e621087ff3
Rahul Purohit <rahulatff@gmail.com>
2018-12-13 15:25:40 +0530

    Dynamic Burger MAking

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import "./Burger.css";
 import BurgerIngredient from "./Burgeringredient/Burgeringredient";
 
 const burger = props => {
+  const transformedIngredient = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + 1} type={igKey} />;
+    });
+  });
+
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredient}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
